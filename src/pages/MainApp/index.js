726b7a5088a784d855c4ef23a3c2070b3910e9f7
hugotaniady11 <hugo.hugoboz@yahoo.com>
2023-03-08 13:06:07 +0700

    Develop Header and Footer, install axios

--- src/pages/MainApp/index.js
+++ src/pages/MainApp/index.js
@@ -7,21 +7,24 @@ import Analytics from '../Analytics'
 import Expenses from '../Expenses'
 import Invoices from '../Invoices'
 import Resources from '../Resources'
+import Members from '../Members';
+import { Header, Footer } from '../../components'
 
 const MainApp = () => {
   return (
     <div>
-        <p>Header</p>
+        <Header />
             <Routes>
                 <Route path="/analytics" element={<Analytics />} />
                 <Route path="/expenses" element={<Expenses />} />
                 <Route path="/invoices" element={<Invoices />} />
                 <Route path="/resources" element={<Resources />} />
                 <Route path="/projects" element={<Projects />} />
+                <Route path="/members" element={<Members />} />
                 <Route path="/tasks" element={<Tasks />} />
                 <Route path="/" element={<Home />} />
             </Routes>
-        <p>Footer</p>
+              <Footer />
     </div>
   )
 }
