d3896083e20adedf10484ebc824bfcc0a2b1f941
hugotaniady11 <hugo.hugoboz@yahoo.com>
2023-05-08 10:41:38 +0700

    Update routes

--- src/config/Routes/index.js
+++ src/config/Routes/index.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Login, Register, MainApp, HomeMain, Home, HomePage, } from '../../pages';
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, redirect  } from 'react-router-dom'
+import { Login, Register, MainApp, HomeMain, Home, HomePage } from '../../pages';
+import { getCurrentUser } from '../../utils/data';
 
-const indexRoutes = () => {
+
+const IndexRoutes = () => {
+  const [currentUser, setCurrentUser] = useState(undefined);
   
   
   return (
     <>
       <Router>
-          <Routes>
-              <Route path="/login" element={<Login/>} />
-              <Route path="/register" element={<Register/>} />
-              <Route path="/homemain" element={<HomeMain/>} />
-              <Route path="*" element={<MainApp/>} />
+        <Routes>
+          {currentUser ? (
+            <Route path="*" element={<MainApp />} />
+          ) : (
+            <> 
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/homemain" element={<HomeMain />} />
               <Route path="/home" element={<Home/>} />
               <Route path="/homepage" element={<HomePage/>} />
-          </Routes>
+            </>
+          )}
+        </Routes>
       </Router>
   
     </>
